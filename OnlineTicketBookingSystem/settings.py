b0c6aa81e3399776deb4eff93de32b855016bdca
princenzmw <princenzmw@gmail.com>
2024-02-07 10:24:28 +0200

    Updated settings

--- OnlineTicketBookingSystem/settings.py
+++ OnlineTicketBookingSystem/settings.py
@@ -4,6 +4,10 @@ Django settings for OnlineTicketBookingSystem project
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2=yv4^7q543$4am^h5bjt468s_p=&@(anbs+05n53wnky!-4sq"
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
